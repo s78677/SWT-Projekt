b5c949db54616044163dc945f74ec69f3a634f6a
s78677 <s78677@bht-berlin.de>
2023-06-26 11:54:03 +0200

    StartController exit Method

--- src/main/java/Controller/StartController.java
+++ src/main/java/Controller/StartController.java
@@ -1,4 +1,10 @@
 package Controller;
 
+import View.Main;
+import javafx.event.ActionEvent;
+
 public class StartController {
+    public void exit(ActionEvent actionEvent) {
+        Main.startStage.close();
+    }
 }
